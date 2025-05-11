c8e0acf868a8f4dc53cb8953d17187e48a28abb7
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-05-10 23:22:14 -0400

    Create a bigram look up tensor for stats.

--- build_makemore.py
+++ build_makemore.py
@@ -20,3 +20,16 @@ for w in words:
 
 sorted(b.items(), key = lambda kv: kv[1], reverse=True) # sorted applies the kv function to each element of the list we pass 
 
+import torch
+N = torch.zeros((28,28), dtype=torch.int32)
+chars = sorted(list(set(''.join(words))))
+stoi = {s:i for i,s in enumerate(chars)}
+stoi['<S>'] = 26
+stoi['<E>'] = 27
+
+for w in words:
+    chs = ['<S>'] + list(w) + ['<E>'] 
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        N[ix1, ix2] += 1
