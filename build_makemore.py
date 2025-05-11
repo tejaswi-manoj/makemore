105d5374af5442623b697321b69ba908f1049de0
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-05-10 22:35:22 -0400

    Create a bigram look up dictionary for stats.

---
+++ build_makemore.py
@@ -0,0 +1,22 @@
+words = open('names.txt', 'r').read().splitlines() # get a list of all the names in the file
+
+# basic information about the dataset - reading and exploring
+length = len(words)
+smallest_word = min(len(w) for w in words) 
+longest_word = max(len(w) for w in words) 
+
+
+# Bigram Language Model: one character predicts the next based on a lookup table of counts
+# Simplest way: Count how often any one of the combinations occurs in the dataset. Need a dictionary for this.
+# 3 things we want to check: Which are the most common starting letters? Most common ending letters? Most common letters occuring together
+b = {}
+for w in words:
+    chs = ['<S>'] + list(w) + ['<E>'] # first and last characters are special characters
+    for ch1, ch2 in zip(chs, chs[1:]):
+        bigram = (ch1, ch2)
+        b[bigram] = b.get(bigram, 0) + 1
+
+# Next, we sort the entire dictionary according to the most commonly occuring bigrams
+
+sorted(b.items(), key = lambda kv: kv[1], reverse=True) # sorted applies the kv function to each element of the list we pass 
+
